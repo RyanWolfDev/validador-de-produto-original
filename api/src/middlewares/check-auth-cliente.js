a6c94855746b7e18ad95e69b59492bc55bf9e3c8
Lucas Cotrim <lucascotrim@Lucass-MacBook-Pro.local>
2020-09-16 00:03:47 -0300

    Tirado Console.log

--- api/src/middlewares/check-auth-cliente.js
+++ api/src/middlewares/check-auth-cliente.js
@@ -7,7 +7,6 @@ module.exports = async (req, res, next) => {
         const token = await req.headers.authorization.split(" ")[1];
         const decodedToken = await jwt.verify(token, 'SECRET_KEY_FOR_CLIENTE');
 
-        console.log(decodedToken)
         req.userData = {
             email: decodedToken.email,
             id: decodedToken.id
